d3ab4ca26a7feabbf4fa0411249688e89330ed14
anthony.son <anthony.son@kakaoent.com>
2024-01-11 17:46:44 +0900

    effective item6

---
+++ src/main/java/effectivejava/item6/Item.java
@@ -0,0 +1,9 @@
+package effectivejava.item6;
+
+public class Item {
+
+    public static void main(String[] args) {
+        Boolean b1 = Boolean.valueOf("true");
+        System.out.println("b1 = " + b1);
+    }
+}
